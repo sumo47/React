ae6c3a59b485e93ee877a17add4ac55b032e8f63
sumit kumar <sumokhan76342@gmail.com>
2023-03-25 10:52:01 +0530

    Prop_types, default props , module

---
+++ my-react-app/src/module.mjs
@@ -0,0 +1,3 @@
+import name,{b,c,d} from './module2.mjs';
+
+console.log(name,b,c,d)
---
+++ my-react-app/src/module2.mjs
@@ -0,0 +1,9 @@
+const a = "Vinti";
+const b = "Sumit";
+const c = "Tushar";
+const d = "Pritam";
+
+export default a;   //we can import as a variable
+export{b}           //The way to export -2nd way
+export{c}
+export{d}
