0ffed49b8b36b31b47800d05f3ea231c7af92100
sumit kumar <sumokhan76342@gmail.com>
2023-04-13 21:08:04 +0530

    camelCase function added

--- my-react-app/src/components/Convert.js
+++ my-react-app/src/components/Convert.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 
 export default function Convert() {
 
-
     function UPPER() {
+
         let newText = text.toUpperCase()
         setText(newText)
     }
@@ -11,6 +11,36 @@ export default function Convert() {
         let newText = text.toLowerCase()
         setText(newText)
     }
+    function clear() {
+        setText("")
+    }
+    // function mix (){
+    //     let array = text.split(" ")
+    //     let finalArray = []
+    //     let arrayElement = array.map(word=>{
+    //         let newArray = word.split("")
+    //         let upperCaseElement =  newArray[0].toUpperCase()
+    //         newArray.splice(0,1,upperCaseElement)
+    //         let joined = newArray.join('')
+    //         finalArray.push(joined)
+    //     })
+    //     let mix = finalArray.join(' ')
+    //     setText(mix)
+    // }
+
+    function CamelCase() {
+        
+        if (text.split(" ")[0] === "") {    //! important to Understand
+            setText("provide Text first!")
+        }
+        else {
+            let upperCaseElement = text.split(' ')[0].split('')[0].toUpperCase()
+            let firstElement = text.split(' ')[0].split('')[0]
+            let newText = text.replace(firstElement, upperCaseElement)
+            setText(newText, upperCaseElement, firstElement)
+        }
+    }
+
     const [text, setText] = useState('')
 
     return (
@@ -23,15 +53,19 @@ export default function Convert() {
                 <textarea id='my-box' className='form-control' rows="8" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text here....'></textarea>
             </div>
             <br />
+
             <button className='btn btn-primary' onClick={UPPER}>UPPERCASE</button>
             <button className="btn btn-primary mx-3" onClick={lower}>lowercase</button>
+            <button className='btn btn-outline-danger' onClick={clear}> clear </button>
+            <button className='btn btn-outline-secondary mx-3' onClick={CamelCase}>Camelcase </button>
+
             <h1>Your Text summary</h1>
             <p className='bg-body-tertiary'>{text.split(" ").length} words and {text.length} characters</p>
             <div className="container bg-light">
-                <p>{0.008*text.split(" ").length}minutes to read</p>
+                <p>{0.008 * text.split(" ").length} minutes to read</p>
             </div>
             <h2>Preview</h2>
-            <div className="container" style={{color:"#f00707", background:"#d7f7f3"}}>
+            <div className="container" style={{ color: "#f00707", background: "#d7f7f3" }}>
                 <p>{text}</p>
             </div>
         </div>
